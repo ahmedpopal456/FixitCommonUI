10aaaab87c928d71253c029cb8f779b9a4c1772d
ThierryRomain <thierryr@orphic.ca>
2020-11-18 13:58:24 -0500

    implemented spacer atom component

---
+++ src/components/atoms/spacer/style.ts
@@ -0,0 +1,30 @@
+import { Platform } from 'react-native';
+import styled from "styled-components/native";
+
+/** PROPS */
+interface Props {
+    stretch?: boolean;
+    width?: string;
+    height?: string;
+}
+
+/** Styled Components */
+export const StyledSpacer = styled.View`
+    ${(props: Props) =>
+        props.stretch &&
+        `
+        flex:1;
+     `}
+     ${(props: Props) =>
+        props.width &&
+        `
+        flexBasis:unset;
+        flexGrow:0;
+        width:${props.width};
+     `}
+     ${(props: Props) =>
+        props.height &&
+        `
+        height:${props.height};
+     `}
+`;
