998d94104518f188ef26a8f06e19a14c624b834f
ThierryRomain <thierryr@orphic.ca>
2020-11-24 14:01:25 -0500

    wrote tests for divider atom #231

--- __tests__/Divider-test.tsx
+++ __tests__/Divider-test.tsx
@@ -1,12 +1,23 @@
 import "react-native";
   import React from "react";
   import { Divider } from "../src/components/atoms/Divider";
-  
+
   // Note: test renderer must be required after react-native.
   import renderer from "react-test-renderer";
   import { render, fireEvent } from '@testing-library/react-native';
-  
+
   it('renders correctly', () => {
-      renderer.create(<Divider required></Divider>);
+      renderer.create(<Divider></Divider>);
   });
-  
+
+  it('renders correctly when text is passed', () => {
+      renderer.create(<Divider>some text</Divider>);
+  });
+
+  it('renders correctly when small', () => {
+      renderer.create(<Divider small></Divider>);
+  });
+
+  it('renders correctly when faded', () => {
+      renderer.create(<Divider small faded>some text</Divider>);
+  });
