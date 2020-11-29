63fcdedbe177df7ff624f293f9573ee8b0be4ce8
ThierryRomain <thierryr@orphic.ca>
2020-11-29 18:26:15 -0500

    Clarified badge props comments #225

--- src/components/atoms/Badge/BagdeModel.ts
+++ src/components/atoms/Badge/BagdeModel.ts
@@ -1,14 +1,14 @@
 import colors from "../../../theme/colors";
 
   export interface BadgeProps {
-      /** Can be any color from the theme color palette */
+      /** Text color of the badge */
       color?: keyof typeof colors;
-      /** Can be any color from the theme color palette */
+      /** Border color of the badge */
       border?: keyof typeof colors;
-      /** Can be any color from the theme color palette */
+      /** Background color of the badge */
       background?: keyof typeof colors;
       /** Float between 0 and 1 */
       opacity?: number;
-      /** Children which will be contained in the badge */
+      /** Child elements which will be contained in the badge */
       children: string;
   }
