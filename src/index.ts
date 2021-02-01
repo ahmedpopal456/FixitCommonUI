7e22eb944e3f0e0321f433cd2b4f73321ed2d34e
Thierry R. Le Bourdais <thierry.lebourdais@gmail.com>
2021-02-01 01:18:21 +0000

    Merged PR 144: (#467) Fixed named color export
    Related work items: #467

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ export * from "./components/atoms/Spacer";
 export * from "./components/atoms/Divider";
 export * from "./components/atoms/Badge";
 export * from "./components/molecules/DonutChart";
-export {default as colors} from "./theme/colors";
 export * from "./components/atoms/Avatar";
+import colors from "./theme/colors";
+export {colors};
